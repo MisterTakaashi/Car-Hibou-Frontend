216f309d59337413543a31993e12c845d2571282
Colin Decomis <defdruil@gmail.com>
2018-03-08 17:51:27 +0100

    login et inscription finalisé, reste interfacage avec API

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalSessionService } from '../shared/services/local-session.service';
+import { LoginService } from '../shared/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit {
+  public login: string = "";
+  public password: string = "";
+  public password2: string = "";
+  public mail: string = "";
+  public errorMessage = false;
 
-  constructor() { }
+  constructor(
+    private _localSessionService: LocalSessionService,
+    private _loginService: LoginService,
+    private _routerService: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  public onClick(){
+    if (this.registrationIsOk())
+    {
+      this._loginService.Register(this.login, this.password, this.password2, this.mail).subscribe(response => {
+            if (response != null){
+              this._localSessionService.setUser(response);
+              this._localSessionService.setToken(response.token);
+              this._routerService.navigate(['/']);
+            }
+          });
+    } else {
+        this.errorMessage = true;
+    }
+  }
+
+  public registrationIsOk(){
+    // TODO Test for values
+    return true;
+  }
 }
