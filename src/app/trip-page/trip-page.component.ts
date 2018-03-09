842ed6bdaa3a7d21dca30d4c5a9f1d765ddf7fad
akitl <akitl1@yahoo.fr>
2018-03-09 15:32:51 +0100

    ajout de la page Trip

---
+++ src/app/trip-page/trip-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-trip-page',
+  templateUrl: './trip-page.component.html',
+  styleUrls: ['./trip-page.component.scss']
+})
+export class TripPageComponent implements OnInit {
+
+
+  constructor(
+    private _routerService: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  public onClick(){
+   
+  }
+
+}
